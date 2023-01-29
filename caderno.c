832ba817fdb63602ebb5b8d687d78524b76b0c7f
JoanaSVSousa <105821815+JoanaSVSousa@users.noreply.github.com>
2023-01-29 18:23:58 +0000

    Create caderno.c

---
+++ caderno.c
@@ -0,0 +1,20 @@
+// No início de cada ficheiro colocasse sempre as libraries necessárias para coisas como o printf (stdio) ou o get_int (cs50) funcionarem.
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+
+// FUNÇÕES
+
+//Após as libraries podem ser colocadas todas as funções que iremos anunciar que existem.
+
+float calc_hours(int hours[], int weeks, char output);
+
+int main (void)  // main é a função principal. Uma função pode não conter inputs (void) ou até 3.
+{ 
+printf("Hello World.");
+}
+
+// VARIABLES
+
+// LOOPS
+// ARRAYS
